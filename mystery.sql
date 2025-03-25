687980425e4aefb983869cc12715184ba8ef0965
LuanaLopez20 <luanaslopez@outlook.com>
2025-03-25 14:50:26 -0300

    Juego misterio

---
+++ mystery.sql
@@ -0,0 +1,17 @@
+SELECT * FROM crime_scene_report
+WHERE date = 20180115 AND type = 'murder' AND city like 'sql city'
+
+
+date			
+20180115	
+
+type	
+murder
+
+description
+Security footage shows that there were 2 witnesses. 
+The first witness lives at the last house on "Northwestern Dr". 
+The second witness, named Annabel, lives somewhere on "Franklin Ave".
+
+city
+SQL City
