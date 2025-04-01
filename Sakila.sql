32f65e7399bbd9524de41f5fe22769a886aa398f
LuanaLopez20 <luanaslopez@outlook.com>
2025-04-01 15:31:28 -0300

    Ejercicio 2

--- Sakila.sql
+++ Sakila.sql
@@ -10,3 +10,14 @@ INNER JOIN city c on a.city_id = c.city_id
 INNER JOIN country cy on c.country_id = cy.country_id
 
 WHERE title = "AFRICAN EGG"
+
+/*EJERCICIO 2*/
+
+SELECT f.rating, ca.name, l.name, f.title
+FROM film f
+INNER JOIN inventory i on  i.film_id = f.film_id
+INNER JOIN language l  on  f.language_id = l.language_id
+INNER JOIN film_category fc on f.film_id = fc.film_id
+INNER JOIN category ca on  fc.category_id = ca.category_id
+
+WHERE f.length BETWEEN 60 and 120
