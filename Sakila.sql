7cbec17d4b339d4f9ea7e28a803f52914c5d0c2c
LuanaLopez20 <luanaslopez@outlook.com>
2025-04-08 16:39:39 -0300

    Ejercicio del 6 al 12 terminados

--- Sakila.sql
+++ Sakila.sql
@@ -40,7 +40,6 @@ FROM film f
 INNER JOIN inventory i on  i.film_id = f.film_id
 INNER JOIN rental r on i.inventory_id = r.inventory_id
 
-
 /*EJERCICIO N5 HECHO CON KAREN*/
 
 SELECT * FROM category ca
@@ -59,3 +58,64 @@ INNER JOIN customer cu on st.customer_id = cu.customer_id
 INNER JOIN address a on s.address_id = a.address_id
 INNER JOIN city c on a.city_id = c.city_id
 INNER JOIN country cy on c.country_id = cy.country_id
+
+/*EJERCICIO 6*/
+
+SELECT COUNT(*) rating FROM film
+GROUP BY rating
+
+/*EJERCICIO 7*/
+
+SELECT COUNT(*) as cant_pelis,ca.name FROM film f
+INNER JOIN film_category fc on f.film_id = fc.film_id 
+INNER JOIN category ca on ca.category_id = fc.category_id
+GROUP BY ca.category_id
+
+/*EJERCICIO 8*/
+
+SELECT COUNT(*) AS cant_actores, a.first_name, a.last_name FROM film f
+INNER JOIN film_actor fa on f.film_id = fa.film_id 
+INNER JOIN actor a on a.actor_id = fa.actor_id 
+GROUP BY a.actor_id
+ORDER BY cant_actores DESC LIMIT 10;
+
+
+/*EJERCICIO 9*/
+
+SELECT COUNT(i.inventory_id) AS total_inventory,  a.address, c.city, co.country
+FROM store s
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON a.city_id = c.city_id
+INNER JOIN country co ON c.country_id = co.country_id
+INNER JOIN inventory i ON s.store_id = i.store_id
+GROUP BY s.store_id;
+
+/*EJERCICIO 10*/
+
+SELECT COUNT(*) AS total_inventory,  a.address, c.city, co.country
+FROM film f
+
+INNER JOIN inventory i ON f.film_id = i.film_id
+INNER JOIN store s on s.store_id = i.store_id
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON a.city_id = c.city_id
+INNER JOIN country co ON c.country_id = co.country_id
+
+GROUP BY s.store_id;
+
+/*EJERCICIO 11*/
+
+SELECT c.name AS categoria, AVG(f.rental_rate * f.rental_duration)
+FROM category c
+INNER JOIN film_category fc ON c.category_id = fc.category_id
+INNER JOIN film f ON fc.film_id = f.film_id
+GROUP BY  c.name
+ORDER BY f.rental_rate DESC;
+
+/*EJERCICIO 12*/
+
+SELECT (f.rental_duration * f.rental_rate), f.rental_rate, r.return_date
+FROM film f
+INNER JOIN inventory i on f.film_id = i.film_id
+INNER JOIN rental r on i.inventory_id = r.inventory_id
+WHERE f.title = "ALABAMA DEVIL"
